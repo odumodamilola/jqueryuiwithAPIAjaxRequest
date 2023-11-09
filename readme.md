85677cd304e3a25fe5aa5f4cbbbf9bc69c7ba57e
odumodamilola <odumodamilolapeacejamb2020@gmail.com>
2023-11-09 04:39:06 +0100

    Jquery ui autocomplete tags with api ajax request

---
+++ readme.md
@@ -0,0 +1,34 @@
+# jQuery UI Autocomplete with API Request
+
+![jQuery](https://img.shields.io/badge/jQuery-3.6.0-blue) ![jQuery UI](https://img.shields.io/badge/jQuery%20UI-1.12.1-orange)
+
+A jQuery UI Autocomplete project that demonstrates how to implement an autocomplete feature using AJAX requests to the "api.randomname" API. This allows users to easily search for and select random names in a user-friendly manner.
+
+## Demo
+
+You can view a live demo of this project [here](#insert-live-demo-url).
+
+![Demo GIF](/assets/demo.gif)
+
+## Features
+
+- Real-time autocomplete suggestions as you type.
+- Fetches data from "api.randomname" for random name suggestions.
+- Elegant and responsive user interface using jQuery UI.
+- Minimal and clean codebase for easy integration into other projects.
+
+## Getting Started
+
+### Prerequisites
+
+To run this project locally, you'll need the following:
+
+- [jQuery](https://jquery.com/)
+- [jQuery UI](https://jqueryui.com/)
+
+### Installation
+
+1. Clone this repository to your local machine:
+
+   ```bash
+   git clone https://github.com/yourusername/jquery-ui-autocomplete.git
--- search.js
+++ search.js
@@ -1,18 +1,35 @@
-const tags = [
-  "html",
-  "css",
-  "bootstrap",
-  "javascript",
-  "php",
-  "laravel",
-  "api",
-];
+const tags = [];
 
-$("#search").autocomplete({ source: tags });
-function looper() {
-let html = "test";
-    $.each(tags, (i, val) => {
-        html += `<span>${val}</span> -`;
-    })
-    $(".output").html(html);
+$("#search").autocomplete({ source: tags, minLenght: 3 });
+
+const url = "https://api.randomuser.me/";
+const results = "?results=100";
+$("#search").keyup(() => {
+  console.log("test");
+  $.ajax({
+    url: url + results,
+    dataType: "json",
+    success: (data) => {
+      // console.log(data.results);
+      //   looper(data.results);
+      $.each(data.results, (i, val) => {
+        // console.log(val.name.first);
+        const first = val.name.first;
+        if ($.inArray(first, tags) == -1) {
+          tags.push(first);
+          $(".output").append(first + '');
+        } else {
+            console.log(first)
+        }
+      });
+    },
+  });
+});
+
+function looper(data) {
+  let html = "test";
+  $.each(tags, (i, val) => {
+    html += `<span>${val}</span> -`;
+  });
+  $(".output").html(html);
 }
